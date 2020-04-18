5633763aee6339431327dd3703b4673d0b21f931
Vitalii_Dadyverin <vetaldadyverin@gmail.com>
2020-04-18 15:57:36 +0300

    Fetch from Server

--- React/confusion/src/redux/configureStore.js
+++ React/confusion/src/redux/configureStore.js
@@ -5,7 +5,6 @@ import {Dishes} from './dishes';
 import {Comments} from './comments';
 import {Leaders} from './leaders';
 import {Promotions} from './promotions';
-import { LEADERS } from '../shared/leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
